60f079b5a4d60a5d2de62b6d19bb75434e5e844f
jacobsa20 <jacobsa20@up.edu>
2024-02-08 20:12:34 -0600

    Second commit

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-//import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { useState} from 'react';
